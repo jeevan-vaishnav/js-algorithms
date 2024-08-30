0e5b725c1212eb0f820421bcbfb7fb05c25680e1
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-08-30 23:35:41 +0530

    Program 1

---
+++ src/js/challanges1/program1.js
@@ -0,0 +1,22 @@
+/*
+ * Q1: Write a function that accepts a string inputand returns a reversed copy 
+ * Creating solutions off the top of my head, i can come up with three ish soltuions, different approches
+ */
+
+/**
+ * Who Cares
+ * It's important to have  a precise vocabulary to talk about how our code performs,
+ * Useful for dicussing trade offs between different approches 
+ * When your code slows down or crashes , identifying parts of the code that are inefficent can help us find pain pints n our applications.
+ * Less important: it comes up in interviews!
+ */
+
+
+
+
+
+
+
+
+
+
