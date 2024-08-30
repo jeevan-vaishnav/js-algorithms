5705858a9a6a2e26ee1bbc76785c4d60db80b90b
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-08-30 23:59:49 +0530

    Adding two approchase to cals sum to N

---
+++ src/js/challanges1/program2.js
@@ -0,0 +1,26 @@
+/**
+ * An Example 
+ * Suppose we want to write a function that calcuates the sum of all numbers from 1up to (and including) some number (n)
+ * 
+ */
+
+/**Solution 1: This is the one that comes to my mind first is to basically  create a total variables accumulator and loop
+ * through all those numbers and add them in one at a time, starting at one the way up until, so that's what i've done here
+ */
+
+// Approach 1
+function addUpTo(n) {
+    let total = 0;
+    for (let i = 0; i <= n; i++) {
+        total += i;
+    }
+    return total
+} 
+console.log("Total number is: " + addUpTo(5));
+
+// Approach 2
+function addUpTo2(n) {
+    return n * (n + 1) / 2;
+}
+console.log("Total number is: " + addUpTo2(265));
+
