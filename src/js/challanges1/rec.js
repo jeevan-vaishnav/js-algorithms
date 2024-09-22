d2961b6b9c12df9cb8d2b8f8231700a5129f6f2a
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-09-22 23:27:21 +0530

    added story for understing the recursion method

---
+++ src/js/challanges1/rec.js
@@ -0,0 +1,40 @@
+/**
+Recursion: Martin & The Dragon
+Once upon a time, Martin found himself face-to-face with an imposing dragon. Desperately, he asked:
+
+Martin: “Excuse me, Mr. Dragon, are any of these numbers odd?”
+(He held out a list: 3142, 5798, 6550, 5914.)
+
+The dragon, looking bored and slightly annoyed, replied:
+Dragon: “I’ll only tell you if the first number is odd.”
+
+Martin: “But I need to know if any of the numbers in the list are odd!”
+Dragon: “Sorry, I’ll only tell you about the first number.”
+
+Martin thought for a moment, then responded:
+Martin: “Fine... what about the first number in this list?”
+(He pointed to 3142.)
+
+Dragon: “Not odd.”
+Martin: “Okay, then what about the first number in this new list?”
+(5798, 6550, 5914.)
+
+Dragon: “Not odd.”
+Martin: “And this list?”
+(6550, 5914.)
+
+Dragon: “Still not odd.”
+Martin: “And this?”
+(5914.)
+
+Dragon: “Not odd.”
+
+Finally, Martin showed the dragon an empty list.
+Martin: “Now what about this list?”
+Dragon: “That’s an empty list, you fool! There’s no number to check!”
+Martin: “Aha! So all the numbers must be even!”
+Dragon: “I never said that…”
+
+As Martin walked away, he realized:
+Martin: “Wait… this whole thing was recursion!”
+ */
