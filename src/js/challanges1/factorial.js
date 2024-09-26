8d2a294bbe45436cd3f41f69374571e9ea87c292
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-09-26 23:07:16 +0530

    recursive function

--- src/js/challanges1/factorial.js
+++ src/js/challanges1/factorial.js
@@ -12,3 +12,20 @@ function fact(n) {
 }
 
 console.log(fact(5));
+
+// Recursivye factorial function 
+
+function factRec(num) {
+    if (num === 1) return 1;
+
+    return num *= factRec(num - 1);
+    /**
+     * return 5 * fac(4)
+     * return 4 * fac(3)
+     * return 3 * fec(2)
+     * return 2 * fec(1)
+     * 1*2 2*3 3*4 4*5
+     */
+}
+
+console.log(factRec(6));
