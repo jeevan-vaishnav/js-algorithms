0fa8558e3656bee8bcd10233d7dc8d511a5a6fd3
Jeevan Vaishnav <jeevanvaishnavv@gmail.com>
2024-09-28 14:25:18 +0530

    added helper cursion method

---
+++ src/js/challanges1/halperMethodCursion.js
@@ -0,0 +1,22 @@
+function checkOddValues(arr) {
+    console.log("array slice: ", arr.slice(1))
+    let result = [];
+
+    function inputHandler(helperInput) {
+        if (helperInput.length === 0) return
+        console.log(helperInput)
+        console.log(helperInput[0])
+        if (helperInput[0] % 2 === 0) {
+            result.push(helperInput[0])
+        }
+
+        inputHandler(helperInput.slice(1))
+    }
+
+    inputHandler(arr);
+
+    return result;
+}
+
+
+console.log(checkOddValues([1, 2, 3, 4, 5, 6]))
