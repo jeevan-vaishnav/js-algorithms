a394334611f9df9ad351890b2c4e36da7fe8308c
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2025-10-06 16:52:36 +0000

    js intro

---
+++ src/js/geeksforgeeks.org/js.js
@@ -0,0 +1,39 @@
+console.log("Intro OF JS ")
+/**
+ * JavaScript is varstile,dynamic,programing langauge them brings life to web page by making them interactive
+ * It is used to building attactive applications
+ * Support both client and server
+ * Integrated seamlessly with html, css and reach standard library
+ * 
+ * Its Object Oriented,Platform independent,Update HTML using the DOM,Capable Of Expcetion Handling,Allows async function
+ * Java Script is single threaded languages that executed one task at time
+ * It is an interpreted language which means it executes the code line by line
+ * The Data type of the varibale is decided at run time in JS, which is why it is called dynamically typed.
+ */
+
+/**
+ * Hello World, Program in server code
+ */
+console.log("Hello World! -> Server Side")
+/**
+ * Key Features in JS
+ * Client Side Scripting: JS runs on the user's browser, so has faster response time 
+ * Verstile: Can be use for wide range task, from simple calculation to server side application
+ * Event Driven: Responds to user action (clicks, keystrokes) in real time
+ * Asynchronous: It can handle task like fetching data from servers w/o freezing the user interface
+ * Rich Echosystem: There are numerous libraries and framework built on js, such as react angular and vuejs , which make development faster and more efficent
+ */
+
+/**
+ * JS Verions
+ * ES5 -> 2009 -> strict mode,JSON,getters/setters
+ * ES6 -> 2015 -> let/const/classes,arrow,functions
+ * ES7-ES13 -> 2016-2022 -> async/await/BigInt,optional chaning
+ * ES14 -> 2023 -> toSorted,findLast,static,blocks
+ * 
+ * Note: Older Version of browser do not support ES6
+ */
+
+
+
+
