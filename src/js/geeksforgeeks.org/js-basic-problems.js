084799cc0a91b74482d8577e1e2cff1ec15d88ad
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2025-10-08 09:37:56 +0000

    linear search

--- src/js/geeksforgeeks.org/js-basic-problems.js
+++ src/js/geeksforgeeks.org/js-basic-problems.js
@@ -59,4 +59,55 @@ function getAlternates(array) {
 
 let tempArra = [10, 20, 30, 40, 50]
 let getResult = getAlternates(tempArra);
-console.log(getResult.join(" ")) 
+console.log(getResult.join(" "))
+
+console.log("Linear Search Algorithm")
+/**
+ * Given an array, arr[] of n integers, and an interger element x,
+ * find whether element x is present in the array. return the index
+ * of the first occurrence of x in the array . or -1 if it doesn't exits
+ */
+
+
+/**
+ * Input arr[] = [1,2,3,4], x = 3
+ * Output 2
+ * 
+ * Explanation:There is on test case with array as [1,2,3,4] and 
+ * element to be searched as 3. 
+ * Since 3 is present at index 2, the
+ * output is 2
+ * 
+ * IN linear search, we iterate overll all the elements of the array and check if
+ * it the current element is equal to the target element, If we find any
+ * element to be equal to the target element, then return the index of 
+ * the current element, Otherwise, if no element is equal to the target element,
+ * then return -1 as the element is not found Linear search is also knwos as sequential
+ * search
+ */
+
+
+const LinearArray = [5, 6, 7, 8, 9]
+const searchKey = 15;
+console.log("Original Array is :", LinearArray)
+console.log("Search Key is :", searchKey)
+function IndexSearch(arr, x) {
+
+    if (arr.length === 0) {
+        return "No Data Available"
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (x == arr[i]) {
+            return `The Index is found at ${i}`
+        }
+    }
+
+    return - 1
+
+}
+
+const res = IndexSearch(LinearArray, searchKey);
+console.log(res)
+
+
