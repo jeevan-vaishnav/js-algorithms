f4c05e821065823b2737bfd8f4bc89cb5ea030c1
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2026-05-31 18:46:20 +0000

    test pas

--- src/js/searching/index.js
+++ src/js/searching/index.js
@@ -106,11 +106,32 @@ function customFindIndex(users,callback){
             return i
         }
     }
-    return undefined
+    return -1
 }
 
 const resultFindIndexCustom = customFindIndex(users, user => user.id === 2);
 
 console.log("Result Find Index Custom:", resultFindIndexCustom)
 
+
+/**
+ * Linear Search
+ * Pseudocode
+ * This function accepts an array and a value
+ * Loop through the array and check if the current array element is equal tot he value 
+ * If it is, return the index at which the element is found
+ * If the value is never found, return -1
+ */
+function linearSearch(inputArray,tgt){
+  // add whatever parameters you deem necessary - good luck!
+  for(let i = 0; i < inputArray.length; i++){
+    if(inputArray[i] === tgt){
+        return i
+    }
+  }
+  return -1
+}
+const inputValue = [10, 10, 20, 25, 30]
+const targetValue  =  30
+console.log("Result 1:",linearSearch(inputValue,targetValue))
 
